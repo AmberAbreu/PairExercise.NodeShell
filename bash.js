2382aafc681bdfe455a507d3b7706c14a039d24d
Amber Abreu <ambermabreu@gmail.com>
2021-06-21 16:22:19 -0400

    ls and cat

--- bash.js
+++ bash.js
@@ -1,9 +1,24 @@
+let pwdCommand = require('./pwd')
+const fs = require('fs');
+const ls = require('./ls');
+const cat = require('./example/cat')
+
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
+  
 
   if (cmd === 'pwd') {
-    process.stdout.write('${process.cwd()');
+    pwdCommand();
+  }
+
+  else if (cmd === "ls" ){
+    ls()
+  }
+
+  else if (cmd.includes('cat')){
+    const fileName = cmd.split(' ').slice(1).join('')
+    cat(fileName)
   }
 
   process.stdout.write('You typed: ' + cmd);
